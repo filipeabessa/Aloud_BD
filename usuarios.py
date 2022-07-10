79c8911ea4ae28023dce285ebed1133ccb827002
filipeabessa <filipegbessa@gmail.com>
2022-07-10 18:17:02 -0300

    feat: fazer metodo de login setar todas as informacoes de usuario

--- usuarios.py
+++ usuarios.py
@@ -16,6 +16,7 @@ class Usuario:
         self.data_nascimento = ""
         self.foto_perfil = ""
         self.logado = False
+        self.eh_vendedor = False
 
         self.endereco = Endereco(cursor, conexao)
 
@@ -31,7 +32,32 @@ class Usuario:
 
         self.cursor.execute(comando)
 
-        if len(self.cursor.fetchall()) > 0:
+        infos_usuario = self.cursor.fetchall()
+
+        if len(infos_usuario) > 0:
+            self.ID_usuario = infos_usuario[0][0]
+            self.nome = infos_usuario[0][1]
+            self.sobrenome = infos_usuario[0][2]
+            self.email = infos_usuario[0][3]
+
+            print(self.ID_usuario)
+            comando = (
+                f'SELECT * FROM usuario_vendedor WHERE id_usuario = "{self.ID_usuario}"'
+            )
+            self.cursor.execute(comando)
+            infos_usuario_vendedor = self.cursor.fetchall()
+            print(infos_usuario_vendedor)
+
+            if len(infos_usuario_vendedor) > 0:
+                self.eh_vendedor = True
+                self.cpf_cnpj = infos_usuario_vendedor[0][0]
+                self.data_nascimento = infos_usuario_vendedor[0][1]
+                self.foto_perfil = infos_usuario_vendedor[0][2]
+                self.ID_endereco = infos_usuario_vendedor[0][4]
+
+            else:
+                self.eh_vendedor = False
+
             print("Login realizado com sucesso!")
             self.logado = True
         else:
@@ -68,7 +94,9 @@ class Usuario:
             self.nome, self.sobrenome, self.email, self.senha
         )
 
-        self.cursor.execute("SELECT MAX(id_usuario) FROM aloud.usuario")
+        self.cursor.execute(
+            f"SELECT MAX(id_usuario) FROM {self.conexao.database}.usuario"
+        )
         self.ID_usuario = self.cursor.fetchall()[0][0]
 
         cadastrar_info_vendedor = input(
@@ -76,6 +104,7 @@ class Usuario:
         )
 
         if cadastrar_info_vendedor == "2":
+            self.eh_vendedor = True
             print("Cadastro vendedor: \n")
             self.cpf_cnpj = input("CPF/CNPJ:")
             self.data_nascimento = input("Data de nascimento (aaaa-mm-dd): ")
@@ -83,7 +112,9 @@ class Usuario:
 
             self.endereco.cadastrar_endereco()
 
-            self.cursor.execute("SELECT MAX(id_endereco) FROM aloud.endereco")
+            self.cursor.execute(
+                f"SELECT MAX(id_endereco) FROM {self.conexao.database}.endereco"
+            )
             self.ID_endereco = self.cursor.fetchall()[0][0]
 
             self.cadastrar_usuario_vendedor_banco(
