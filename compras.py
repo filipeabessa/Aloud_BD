a94cd7b01e9495341ff7516fc21262cb85731711
filipeabessa <filipegbessa@gmail.com>
2022-07-11 19:10:12 -0300

    feat: criar metodos ocos na classe Compras

---
+++ compras.py
@@ -0,0 +1,15 @@
+class Compras:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Compras"
+
+    def listar_compras(self):
+        pass
+
+    def comprar(self):
+        pass
+
+    def cancelar_compra(self):
+        pass
